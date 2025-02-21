58f43d62d359fe7cf216325a3d59d5b4eeabd996
Georg Schramm (Windows) <georg.schramm@kuleuven.be>
2025-02-21 10:44:01 +0100

    wip phantom

--- sim_phantoms.py
+++ sim_phantoms.py
@@ -40,52 +40,90 @@ def pet_phantom(
 
         x_att = mu_value * xp.astype(x_em > 0, xp.float32)
     elif type == 1:
-        # elliptical phantom
-        x_em = xp.zeros(in_shape, device=dev, dtype=xp.float32)
-        x_att = xp.zeros(in_shape, device=dev, dtype=xp.float32)
-        c0 = in_shape[0] / 2
-        c1 = in_shape[1] / 2
-        c2 = in_shape[1] / 2
-        a = in_shape[0] / 2.5  # semi-major axis
-        b = in_shape[1] / 4  # semi-minor axis
+        oversampling_factor = 4
 
-        rix = in_shape[0] / 25
-        riy = in_shape[1] / 25
+        # elliptical phantom on oversampled grid
+        oversampled_shape = tuple(oversampling_factor * x for x in in_shape)
+        x_em_oversampled = xp.zeros(oversampled_shape, device=dev, dtype=xp.float32)
+        x_att_oversampled = xp.zeros(oversampled_shape, device=dev, dtype=xp.float32)
+        c0 = oversampled_shape[0] / 2
+        c1 = oversampled_shape[1] / 2
+        c2 = oversampled_shape[2] / 2
+        a = oversampled_shape[0] / 2.5  # semi-major axis
+        b = oversampled_shape[1] / 4  # semi-minor axis
 
-        y, x = xp.ogrid[: in_shape[0], : in_shape[1]]
+        rix = oversampled_shape[0] / 25
+        riy = oversampled_shape[1] / 25
+
+        y, x = xp.ogrid[: oversampled_shape[0], : oversampled_shape[1]]
 
         outer_mask = ((x - c0) / a) ** 2 + ((y - c1) / b) ** 2 <= 1
         inner_mask = ((x - c0) / rix) ** 2 + ((y - c1) / riy) ** 2 <= 1
 
-        for z in range(in_shape[2]):
-            x_em[:, :, z][outer_mask] = 3.0
-            x_em[:, :, z][inner_mask] = 0.0
+        for z in range(oversampled_shape[2]):
+            x_em_oversampled[:, :, z][outer_mask] = 3.0
+            x_em_oversampled[:, :, z][inner_mask] = 0.0
 
-            x_att[:, :, z][outer_mask] = mu_value
-            x_att[:, :, z][inner_mask] = mu_value / 3
+            x_att_oversampled[:, :, z][outer_mask] = mu_value
+            x_att_oversampled[:, :, z][inner_mask] = mu_value / 3
+
+        # add a few spheres to the emission image
+
+        x, y, z = xp.ogrid[
+            : oversampled_shape[0], : oversampled_shape[1], : oversampled_shape[2]
+        ]
+
+        r_sp = 3 * [oversampled_shape[2] / 9]
+        r_sp2 = 3 * [oversampled_shape[2] / 17]
+
+        for z_offset in [c2, 0.35 * c2]:
+            sp_mask = ((x - c0) / r_sp[0]) ** 2 + ((y - 1.4 * c1) / r_sp[1]) ** 2 + (
+                (z - z_offset) / r_sp[2]
+            ) ** 2 <= 1
+            x_em_oversampled[sp_mask] = 6.0
+
+            sp_mask2 = ((x - 1.3 * c0) / r_sp[0]) ** 2 + ((y - c1) / r_sp[1]) ** 2 + (
+                (z - z_offset) / r_sp[2]
+            ) ** 2 <= 1
+            x_em_oversampled[sp_mask2] = 1.0
+
+            sp_mask = ((x - c0) / r_sp2[0]) ** 2 + ((y - 0.6 * c1) / r_sp2[1]) ** 2 + (
+                (z - z_offset) / r_sp2[2]
+            ) ** 2 <= 1
+            x_em_oversampled[sp_mask] = 6.0
+
+            sp_mask2 = ((x - 0.7 * c0) / r_sp2[0]) ** 2 + ((y - c1) / r_sp2[1]) ** 2 + (
+                (z - z_offset) / r_sp2[2]
+            ) ** 2 <= 1
+            x_em_oversampled[sp_mask2] = 1.0
+
+        # downsample to original grid size by averaging
+        x_em = xp.zeros(in_shape, device=dev, dtype=xp.float32)
+        x_att = xp.zeros(in_shape, device=dev, dtype=xp.float32)
+
+        for i in range(in_shape[0]):
+            for j in range(in_shape[1]):
+                for k in range(in_shape[2]):
+                    x_em[i, j, k] = xp.mean(
+                        x_em_oversampled[
+                            oversampling_factor * i : oversampling_factor * (i + 1),
+                            oversampling_factor * j : oversampling_factor * (j + 1),
+                            oversampling_factor * k : oversampling_factor * (k + 1),
+                        ]
+                    )
+                    x_att[i, j, k] = xp.mean(
+                        x_att_oversampled[
+                            oversampling_factor * i : oversampling_factor * (i + 1),
+                            oversampling_factor * j : oversampling_factor * (j + 1),
+                            oversampling_factor * k : oversampling_factor * (k + 1),
+                        ]
+                    )
 
         x_em[:, :, :2] = 0
         x_em[:, :, -2:] = 0
 
-        x_att[:, :, :2] = 0
-        x_att[:, :, -2:] = 0
-
-        x_att[:, :, 1][outer_mask] = mu_value
-        x_att[:, :, -2][outer_mask] = mu_value
-
-        # add a few spheres to the emission image
-
-        x, y, z = xp.ogrid[: in_shape[0], : in_shape[1], : in_shape[2]]
-
-        r_sp1 = 3 * [in_shape[2] / 7]
-        r_sp2 = 3 * [in_shape[2] / 16]
-        offset = in_shape[2] // (4 / 3)
-
-        for i, r_sp in enumerate([r_sp1, r_sp2]):
-            sp_mask = ((x - c0) / r_sp[0]) ** 2 + (
-                (y - (c1 + ((-1) ** i) * offset)) / r_sp[1]
-            ) ** 2 + ((z - 15) / r_sp[2]) ** 2 <= 1
-            x_em[sp_mask] = 5.0
+        x_att[:, :, 0] = 0
+        x_att[:, :, -1] = 0
 
     else:
         raise ValueError("Invalid phantom type")
