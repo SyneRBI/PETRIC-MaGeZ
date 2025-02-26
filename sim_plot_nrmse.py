0d5f917ab37c16c51bd67ced84fe3523a84a02c1
Georg Schramm (Windows) <georg.schramm@kuleuven.be>
2025-02-26 09:43:15 +0100

    adapt plot script

--- sim_plot_nrmse.py
+++ sim_plot_nrmse.py
@@ -5,7 +5,7 @@ import numpy as np
 from pathlib import Path
 
 methods = ["SGD", "SAGA", "SVRG"]
-true_counts_list = [3e6, 3e7]
+true_counts_list = [1e7, 1e8]
 beta_rels = [1.0, 4.0, 16.0]
 precond_types = [1, 2]
 
@@ -15,10 +15,10 @@ sim_path.mkdir(exist_ok=True)
 gamma_rdp = 2
 num_iter_bfgs_ref = 500
 num_rings = 17
-tof = False
+tof = True
 contam_fraction = 0.5
 seed = 1
-phantom_type = 1
+phantom_type = 2
 
 num_epochs = 20
 num_subsets = 27
@@ -36,22 +36,40 @@ fig, ax = plt.subplots(
     sharey=True,
 )
 
+fig2, ax2 = plt.subplots(
+    nrows,
+    ncols,
+    figsize=(3 * ncols, 3 * nrows),
+    tight_layout=True,
+    sharex=True,
+    sharey=True,
+)
+
 for axx in ax.ravel():
     axx.axhline(1e-2, color="k")
 
 for i, true_counts in enumerate(true_counts_list):
     for j, beta_rel in enumerate(beta_rels):
         beta = beta_rel * (2e-4) * (true_counts / 3e7)
+
+        ref_file = (
+            sim_path
+            / f"rdp_t_{true_counts:.2E}_b_{beta:.2E}_g_{gamma_rdp:.2E}_n_{num_iter_bfgs_ref}_nr_{num_rings}_tof_{tof}_cf_{contam_fraction}_s_{seed}_ph_{phantom_type}.npy"
+        )
+
+        x_ref = np.load(ref_file)
+        sl = x_ref.shape[2] // 2
+
+        ax2[i, j].imshow(
+            x_ref[..., sl], vmin=0, vmax=0.18 * true_counts / 1e7, cmap="Greys"
+        )
+
         for im, method in enumerate(methods):
             for ip, precond_type in enumerate(precond_types):
-                ref_file = (
-                    sim_path
-                    / f"rdp_t_{true_counts:.2E}_b_{beta:.2E}_g_{gamma_rdp:.2E}_n_{num_iter_bfgs_ref}_nr_{num_rings}_tof_{tof}_cf_{contam_fraction}_s_{seed}_ph_{phantom_type}.npy"
-                )
 
                 res_file = (
                     ref_file.parent
-                    / f"{ref_file.stem}_ne_{num_epochs}_ns_{num_subsets}_m_{method}_pc__{precond_type}_{step_size_func_str}.json"
+                    / f"{ref_file.stem}_ne_{num_epochs}_ns_{num_subsets}_m_{method}_pc_{precond_type}__pf_4.0_{step_size_func_str}.json"
                 )
 
                 # read nrmse_stochastic from the JSON file
@@ -80,10 +98,15 @@ for axx in ax[-1, :]:
 
 for i, axx in enumerate(ax[0, :]):
     axx.set_title(f"beta = {beta_rels[i]:.1f}", fontsize="medium")
+for i, axx in enumerate(ax2[0, :]):
+    axx.set_title(f"beta = {beta_rels[i]:.1f}", fontsize="medium")
 
 for i, axx in enumerate(ax[:, 0]):
     axx.set_ylabel(f"NRMSE true counts: {true_counts_list[i]:.1E}")
+for i, axx in enumerate(ax2[:, 0]):
+    axx.set_ylabel(f"true counts: {true_counts_list[i]:.1E}")
 
 ax[0, 0].legend(fontsize="x-small", ncol=2, loc="lower right")
 
 fig.show()
+fig2.show()
